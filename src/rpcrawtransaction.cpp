4c9f77db3a44a2dfc7bd134f4514f536a44966a1
8bitcoder <8bitcoder@users.noreply.guthub.com>
2016-06-20 11:47:07 +0200

    daemon: MYR to XMY

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -389,7 +389,7 @@ Value createrawtransaction(const Array& params, bool fHelp)
             "     ]\n"
             "2. \"addresses\"           (string, required) a json object with addresses as keys and amounts as values\n"
             "    {\n"
-            "      \"address\": x.xxx   (numeric, required) The key is the myriad address, the value is the MYR amount\n"
+            "      \"address\": x.xxx   (numeric, required) The key is the myriad address, the value is the XMY amount\n"
             "      ,...\n"
             "    }\n"
 
