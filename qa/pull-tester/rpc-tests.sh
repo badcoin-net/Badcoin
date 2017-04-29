88c36e82adde7288bad5cf9140805baa2676946c
cryptapus <info@cryptap.us>
2017-04-29 18:28:53 -0500

    Readme and documentation

--- qa/pull-tester/rpc-tests.sh
+++ qa/pull-tester/rpc-tests.sh
@@ -31,8 +31,6 @@ testScripts=(
 testScriptsExt=(
     'bipdersig-p2p.py'
     'bipdersig.py'
-    'bip65-cltv-p2p.py'
-    'bip65-cltv.py'
     'getblocktemplate_longpoll.py'
     'getblocktemplate_proposals.py'
     'pruning.py'
