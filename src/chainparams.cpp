21a794b34401acc247708b2e121b68f982e5c550
cryptapus <info@cryptap.us>
2018-02-06 16:53:50 -0600

    add regtest MIP2Height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -422,6 +422,7 @@ public:
         consensus.BIP34Hash = uint256();
         consensus.BIP65Height = 1351; // BIP65 activated on regtest (Used in rpc activation tests)
         consensus.BIP66Height = 1251; // BIP66 activated on regtest (Used in rpc activation tests)
+        consensus.MIP2Height = 1;
         consensus.powLimit = uint256S("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = consensus.nPowTargetSpacingV2; // Current value
